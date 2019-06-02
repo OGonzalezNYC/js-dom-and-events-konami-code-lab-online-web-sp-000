47741e79396ccf1755dcdac53a2321e7b07f0a3b
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-06-02 00:24:25 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -4,14 +4,11 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
   document.body.addEventListener('keydown', function(e) {
-    
     const key = parseInt(e.detail || e.which);
-    
     if (key === code[index]) {
       index++;
       if (index === code.length) { 
         alert("Hurray!");
-        
         index = 0;
       }
     } else {
@@ -19,25 +16,5 @@ function init() {
     }
   });
 }
-
-// function init() {
-//   let index = 0;
-
-//   document.body.addEventListener('keydown', function(e) {
-//     const key = parseInt(e.which || e.detail);
-
-//     if (code[index] === key) {
-//       index++;
-
-//       if (index === code.length) {
-//         alert('Hurray!');
-
-//         index = 0;
-//       }
-//     } else {
-//       index = 0;
-//     }
-//   });
-// }
 
 init();
