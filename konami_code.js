46ac9f6a84fb60608c7fab01336e22834313f8d5
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-01-03 00:30:11 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  let index = 0;
+  let i = 0;
   document.body.addEventListener('keydown', function(e) {
     //document.body.addEventListener('keydown', function(event) {
     //const key = parseInt(event.detail || event.which);
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
-      index++;
-      if (index === code.length) {
+      i++;
+      if (i === code.length) {
         alert("Congratulations!")
-        index = 0;
+        i = 0;
       }
     } else {
-      index = 0;
+      i = 0;
     }
   }) 
 }
