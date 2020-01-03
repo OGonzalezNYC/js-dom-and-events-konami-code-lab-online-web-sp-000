7d9b10cb1f48da3487525fd4fb25cab98e8e2b9b
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-01-03 00:31:06 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-  let index = 0;
+  let i = 0;
   document.body.addEventListener('keydown', function(e) {
+    //document.body.addEventListener('keydown', function(event) {
+    //const key = parseInt(event.detail || event.which);
     const key = parseInt(e.detail || e.which);
-    if (key === code[index]) {
-      index++;
-      if (index === code.length) { 
-        alert("Hurray!");
-        index = 0;
+    if (key === code[i]) {
+      i++;
+      if (i === code.length) {
+        alert("Congratulations!")
+        i = 0;
       }
     } else {
-      index = 0;
+      i = 0;
     }
-  });
+  }) 
 }
+
 
 init();
